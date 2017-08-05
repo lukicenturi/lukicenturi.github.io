2ab32062259d69c36345ed9e7371074934bdfc49
Luki Centuri <drybutcool@gmail.com>
2017-08-05 15:40:39 +0700

    test

---
+++ skyangel/js/Plane.js
@@ -0,0 +1,47 @@
+class Plane{
+	constructor(){
+		this.image = new Image();
+		this.image.src = 'sprite/airplane.png';
+		this.x = 20;
+		this.y = 200;
+		this.w = 80;
+		this.h = 50;
+		this.pos = {
+			x:0,
+			y:0,
+			w:308,
+			h:145
+		}
+	}
+
+	move(key){
+		switch(key){
+			case 38 : 
+				if(this.y > 10){
+					this.y -= 20;
+				}
+				break;
+			case 39 : 
+				if(this.x + this.w <= 780){
+					this.x += 20;
+				}
+				break;
+			case 40 : 
+				if(this.y + this.h <= 470){
+					this.y += 20;
+				}
+				break;
+			case 37 : 
+				if(this.x > 10){
+					this.x -= 20;
+				}
+				break;
+		}
+	}
+
+	draw(){
+		ctx.save();
+		ctx.drawImage(this.image, this.pos.x * this.pos.w, this.pos.y * this.pos.h, this.pos.w, this.pos.h, this.x, this.y, this.w, this.h);
+		ctx.restore();
+	}
+}
---
+++ skyangel/js/config.js
@@ -0,0 +1,5 @@
+var canvas = $("#canvas")[0],
+	ctx = canvas.getContext('2d'),
+	cw = 800,
+	ch = 480,
+	speed = 10;
